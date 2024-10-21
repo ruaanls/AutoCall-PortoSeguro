affcefc12348d98dc0c03aa1f5b98244f5d3504b
Ruan Lima <124522882+ruaanls@users.noreply.github.com>
2024-10-21 20:52:33 -0300

    Estilização de cadastro ptonta

--- challenge-fiap/src/app/cadastro/page.tsx
+++ challenge-fiap/src/app/cadastro/page.tsx
@@ -1,56 +1,54 @@
 "use client"
-import React, { useState } from "react";
+import Image from "next/image";
 
-
+import imgCad from "../../img/img-login.png"
 
 export default function Cadastro() {
     
 
     return (
         <main>
-            <div>
-                <div>
-                    {/* <img src={img} alt="" /> */}
+            <div className="cadastroGeral">
+                <div className="containerImg">
+                    <Image src={imgCad} alt="Imagem de um mecânico com camisa azul" className="img-login"/>
                 </div>
-                <div>
-                    <div>
-                        <h1>Crie sua conta Porto</h1>
-                        {/*criarDiv && */<h1> Cpf Já Cadastrado em Sistema</h1>}
-                        {/*criarDivSenhas && */<h1>Suas Senhas não são Iguais, Tente Novamente</h1>}
+                <div className="containerExternoForm">
+                    <div className="containerForm justify-normal w-[100%]">
+                        <h1 className="text-[2.3rem] mb-[3%]">Crie sua conta Porto</h1>
 
-                        <form /*onSubmit={onSubmit(handleSubmit)}*/>
-                            <div>
-                                <div>
+                        <form /*onSubmit={onSubmit(handleSubmit)}*/ className="form">
+                            <div className="linha">
+                                <div className="formulario">
                                     <input type="text" placeholder="CPF" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" title="CPF INVÁLIDO, use este padrão: 111.111.111-11" /*{...register("cpf")}*/ />
                                     <p>{/*errors.cpf?.message*/}</p>
                                 </div>
-                                <div>
+                                <div className="formulario">
                                     <input type="text" placeholder="Nome Completo" /*{...register("nome")}*/ />
                                     <p>{/*errors.nome?.message*/}</p>
                                 </div>
                             </div>
-                            <div>
-                                <div>
+                            <div className="linha">
+                                <div className="formulario">
                                     <input type="text" placeholder="Celular" /*{...register("celular")} *//>
                                     <p>{/*errors.celular?.message*/}</p>
                                 </div>
-                                <div>
+                                <div className="formulario">
                                     <input type="email" placeholder="Email" title="E-MAIL INVÁLIDO, use este padrão: aaaaa@aaaa.aaa.aa" pattern="[^@\s]+@[^@\s]+\.[^@\s]+" /*{...register("email")} *//>
                                     <p>{/*errors.email?.message*/}</p>
                                 </div>
                             </div>
-                            <div>
-                                <div>
+                            <div className="linha">
+                                <div className="formulario">
                                     <input type="password" placeholder="Senha" /*{...register("senha1")} *//>
                                     <p>{/*errors.senha1?.message*/}</p>
                                 </div>
-                                <div>
+                                <div className="formulario">
                                     <input type="password" placeholder="Confirmar Senha" /*{...register("senha2")}*/ />
                                     <p>{/*errors.senha2?.message*/}</p>
                                 </div>
                             </div>
-                            <div>
-                                <button type="submit">Criar Conta</button>
+                            <div className="botao text-center w-[100%] " >
+                                <button type="submit" style={{width: "50%"}}className="w-[100%] py-[20%]">Criar Conta</button>
                             </div>
                         </form>
                     </div>
