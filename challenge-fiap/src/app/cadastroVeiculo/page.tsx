2f3f21e8ad036020deddb7adbd80f71a7a3bc7c9
RichardyBS <editionplays@gmail.com>
2024-10-30 17:55:09 -0300

    Alteracao e criacao do Cadastra Veiculo Com Tailwind e semi preparado para API

--- challenge-fiap/src/app/cadastroVeiculo/page.tsx
+++ challenge-fiap/src/app/cadastroVeiculo/page.tsx
@@ -1,60 +1,115 @@
-import React from 'react'
+// import React, { useState } from 'react';
 
-export default function cadastroVeiculo() {
+import Image from 'next/image';
+import imgLogin from '@/img/img-cadastro-veiculos.png';
+
+export default function CadastroVeiculo() {
+    // const [formData, setFormData] = useState({
+    //     placa: '',
+    //     marca: '',
+    //     modelo: '',
+    //     ano: ''
+    // });
+
+    // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     const { name, value } = e.target;
+    //     setFormData(prevState => ({
+    //         ...prevState,
+    //         [name]: value
+    //     }));
+    // };
+
+    // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault();
+    //     console.log(formData);
+    //     
+    // // bota tua api do java aqui ruanzito (na teoria funfa)
+    // // ou arruma pra encaixar nao sei exatamente como voce fez
+    // };
+
     return (
         <main>
-            <div>
-                <div>
+            <div className="cadastroMain">
+                <div className="cadForm">
                     <h1>Cadastre seu Veículo</h1>
-                    <div>
+                    
+                    <div className="conteudo-cad">
                         <p>
                             Com seu veículo cadastrado em nosso sistema você tem acesso liberado para agendar consertos e fazer um pré-diagnóstico com nossa IA.
                         </p>
                     </div>
-                    
-                    <div>
+
+                    <div className="containerForm-cad">
                         <h1>Cadastro de veículo</h1>
-                        {/*criarDiv && */<h1>Placa já Existe</h1>}
                         
-                        {/* Formulário de cadastro de veículo */}
-                        {/* <form onSubmit={onSubmit(handleSubmit)}> */}
-                        <form>
-                            <div>
-                                <div>
-                                    {/* <input type="text" placeholder="Placa" pattern="^(?:[A-Z]{3}-\d{4}|[A-Z]{3}[0-9][0-9A-Z ][0-9]{2})$" title="PLACA INVÁLIDA, digite sua placa corretamente conforme o padrão detran/Mercosul" {...register("placa")}/> */}
-                                    <input type="text" placeholder="Placa" pattern="^(?:[A-Z]{3}-\d{4}|[A-Z]{3}[0-9][0-9A-Z ][0-9]{2})$" title="PLACA INVÁLIDA, digite sua placa corretamente conforme o padrão detran/Mercosul" />
-                                    {/* <p>{errors.placa?.message}</p> */}
+                        <form className="form-cad">  
+                            {/* onSubmit={handleSubmit} */}
+                            <div className="linhaContainer-cad">
+                                <div className="linha-cad">
+                                    <input 
+                                        type="text" 
+                                        name="placa"
+                                        // value={formData.placa}
+                                        // onChange={handleChange}
+                                        placeholder="Placa" 
+                                        className="placa-cad"
+                                        pattern="^(?:[A-Z]{3}-\d{4}|[A-Z]{3}[0-9][0-9A-Z][0-9]{2})$"
+                                        required
+                                    />
                                 </div>
-                                <div>
-                                    {/* <input type="text" placeholder="Marca" {...register("marca")}/> */}
-                                    <input type="text" placeholder="Marca" />
-                                    {/* <p>{errors.marca?.message}</p> */}
-                                </div>
-                            </div>
-    
-                            <div>
-                                <div>
-                                    {/* <input type="text" placeholder="Modelo" {...register("modelo")}/> */}
-                                    <input type="text" placeholder="Modelo" />
-                                    {/* <p>{errors.modelo?.message}</p> */}
-                                </div>
-                                <div>
-                                    {/* <input type="text" placeholder="Ano" pattern="[0-9]+$" {...register("ano")}/> */}
-                                    <input type="text" placeholder="Ano" pattern="[0-9]+$" />
-                                    {/* <p>{errors.ano?.message}</p> */}
+                                <div className="linha-cad">
+                                    <input 
+                                        type="text" 
+                                        name="marca"
+                                        // value={formData.marca}
+                                        // onChange={handleChange}
+                                        placeholder="Marca"
+                                        required
+                                    />
                                 </div>
                             </div>
-                            
-                            <button type="submit">Cadastrar Veículo</button>
+
+                            <div className="linhaContainer-cad">
+                                <div className="linha-cad">
+                                    <input 
+                                        type="text" 
+                                        name="modelo"
+                                        // value={formData.modelo}
+                                        // onChange={handleChange}
+                                        placeholder="Modelo"
+                                        required
+                                    />
+                                </div>
+
+                                <div className="linha-cad">
+                                    <input 
+                                        type="text" 
+                                        name="ano"
+                                        // value={formData.ano}
+                                        // onChange={handleChange}
+                                        placeholder="Ano"
+                                        pattern="[0-9]+$"
+                                        className="ano-cad"
+                                        required
+                                    />
+                                </div>
+                            </div>
+
+                            <button type="submit">
+                                Cadastrar Veículo
+                            </button>
                         </form>
                     </div>
                 </div>
-    
-                <div>
-                    {/* <img src={img} alt="" /> */}
-                </div>
+            <div className="cadastroImg">
+                <Image 
+                    src={imgLogin} 
+                    alt="Imagem Login" 
+                    className="img-cad"
+                    priority
+                />
+            </div>
             </div>
         </main>
-    )
-    
-};
+    );
+}
