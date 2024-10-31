a4ba48460ee60e7d4854f2ba4106ec352d7ff17d
RichardyBS <editionplays@gmail.com>
2024-10-31 16:55:17 -0300

    Integrantes ja estilizado Faltando apenas os MediaQuary

--- challenge-fiap/tailwind.config.ts
+++ challenge-fiap/tailwind.config.ts
@@ -11,13 +11,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        'border-spin': {
+          '0%': {
+            transform: 'rotate(0deg)',
+          },
+          '100%': {
+            transform: 'rotate(360deg)',
+          },
+        },
+      },
+      animation: {
+        'border-spin': 'border-spin 7s linear infinite',
+        },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
       fontFamily:{
         sans:['Inter', 'sans-serif']
-      }
+      },
     },
   },
   plugins: [typography, forms],
